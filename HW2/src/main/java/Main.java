5d14751a21d75b1bec762c366d6b7e267e16a493
morentu <vlad981125@gmail.com>
2020-04-17 14:56:25 +0300

    Update

---
+++ HW2/src/main/java/Main.java
@@ -0,0 +1,75 @@
+public class Main
+{
+    enum EAction
+    {
+        Sum,
+        Sub,
+        Mult,
+        Div,
+        Mod;
+    }
+
+    public static void main(String[] args)
+    {
+        byte val_byte = 1;
+        short val_short = 2;
+        int val_int = 3;
+        long val_long = 4;
+        char val_char = 'a';
+        float val_float = 6;
+        double val_double = 7; //объявили и инициализировали переменные
+
+        System.out.println("Примеры неявного преобразования");
+        System.out.println("Byte + Short = " + MathAction(EAction.Sum,val_byte, val_short));
+        System.out.println("Short + Int = " + MathAction(EAction.Sum,val_short, val_int));
+        System.out.println("Short + Char = " + MathAction(EAction.Sum,val_short, (int) val_char)); //Наш метод прнимает числовые значения, однако char является символьным значением, потому закостылим явное преобразование в число и ок
+        System.out.println("Int + Long = " + MathAction(EAction.Sum,val_int, val_long));
+        System.out.println("Int + Float = " + MathAction(EAction.Sum,val_int, val_float));
+        System.out.println("Int + Double = " + MathAction(EAction.Sum,val_int, val_double));
+
+        System.out.println("\nМатематические операции. деление целочисленных типов и остаток от деления");
+        System.out.println("Long 4 + int 3 = " + (int)MathAction(EAction.Sum,val_long, val_int) + " пример математической операции");
+        System.out.println("Long 4 / int 3 = " + (int)MathAction(EAction.Div,val_long, val_int) + " Потеря в виду отсечки дробной части"); // тут делаю дополнительно явное преобразование для корректности демонстрации
+        System.out.println("int 9 / int 2 = " + (int)MathAction(EAction.Div,9,2) + " Потеря в виду отсечки дробной части");
+        System.out.println("Long 4 % Int 3 = " + MathAction(EAction.Mod,val_long, val_int));
+
+        val_int = 100; // для далнейшего преведения в char
+
+        System.out.println("\nПреобразование int -> char -> int");
+        System.out.println("Int " + val_int + " to char = " + (char) val_int);
+        System.out.println("char '" + val_char + "' to int = " + (int) val_char);
+
+        val_double = 1.9434;
+
+        System.out.println("\nПриводим double " + val_double + " к int = " + (int)val_double);
+
+
+
+    }
+
+    /**
+     * Математические операции над любыми числовыми типами данных
+     *
+     * @param act - Действие
+     * @param operand_A,operand_B - Сумируемые числовые значения
+     * @return Возвращает сумму в типе double
+     */
+
+    public static <T extends Number, S extends Number> double MathAction(EAction act,T operand_A, S operand_B)
+    {
+        switch (act)
+        {
+            case Sum:
+                return operand_A.doubleValue() + operand_B.doubleValue();
+            case Div:
+                return operand_A.doubleValue() / operand_B.doubleValue();
+            case Sub:
+                return operand_A.doubleValue() - operand_B.doubleValue();
+            case Mult:
+                return operand_A.doubleValue() * operand_B.doubleValue();
+            case Mod:
+                return operand_A.doubleValue() % operand_B.doubleValue();
+        }
+        return 0;
+    }
+}
---
+++ HW2/src/test/java/MainTest.java
@@ -0,0 +1,56 @@
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.Assert.*;
+
+class MainTest
+{
+
+    double delta = 0.001;
+
+    Main _Main;
+    @Test
+    void TEST_MathAction_Sum()
+    {
+        int testVal_A = 20;
+        double testVal_B = 5;
+        double valExpect = 25;
+        assertEquals(_Main.MathAction(Main.EAction.Sum,testVal_A,testVal_B),valExpect,delta);
+    }
+
+    @Test
+    void TEST_MathAction_Div()
+    {
+        int testVal_A = 20;
+        double testVal_B = 5;
+        double valExpect = 4;
+        assertEquals(_Main.MathAction(Main.EAction.Div,testVal_A,testVal_B),valExpect,delta);
+    }
+
+    @Test
+    void TEST_MathAction_Sub()
+    {
+        int testVal_A = 20;
+        double testVal_B = 5;
+        double valExpect = 15;
+        assertEquals(_Main.MathAction(Main.EAction.Sub,testVal_A,testVal_B),valExpect,delta);
+    }
+
+    @Test
+    void TEST_MathAction_Mult()
+    {
+        int testVal_A = 20;
+        double testVal_B = 5;
+        double valExpect = 100;
+        assertEquals(_Main.MathAction(Main.EAction.Mult,testVal_A,testVal_B),valExpect,delta);
+    }
+
+    @Test
+    void TEST_MathAction_Mod()
+    {
+        int testVal_A = 4;
+        double testVal_B = 3;
+        double valExpect = 1;
+        assertEquals(_Main.MathAction(Main.EAction.Mod,testVal_A,testVal_B),valExpect,delta);
+    }
+}
