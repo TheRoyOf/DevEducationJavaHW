d0e4abbaff9eebf6959eddfcbabe804e93691a57
morentu <vlad981125@gmail.com>
2020-05-22 12:23:36 +0300

    update

---
+++ HW8-OOP-2/src/test/java/CalculatorTest.java
@@ -0,0 +1,16 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculatorTest {
+
+    @org.junit.jupiter.api.Test
+    void isNumeric() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void parseValue() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void calc() {
+    }
+}
